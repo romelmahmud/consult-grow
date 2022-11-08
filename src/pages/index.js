732d781329b4eba6e3ebbbd9e8c3b6f4c32ca6ff
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-08 16:31:37 +0600

    Basic folder stucture done

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home/Home";
+import Blog from "./Blog/Blog";
+import Login from "./Login/Login";
+import Register from "./register/Register";
+import Service from "./Service/Service";
+import Services from "./Services/Services";
+import User from "./User/User";
+
+export { Home, Blog, Login, Register, Service, Services, User };
