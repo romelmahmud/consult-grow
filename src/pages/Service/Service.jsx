0fbbd156045477d1fd1b63e3c9445ce764622946
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-09 23:14:01 +0600

    single service page data fetched complete with loading spinner

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Spinner } from "flowbite-react";
 
 const Service = () => {
-  return <div>Service</div>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [serviceData, setServiceData] = useState({});
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://consult-review.vercel.app/service/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setServiceData(data.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+  return loading ? (
+    <div className="py-20  my-5  flex justify-center items-center">
+      <Spinner size="xl" color="success" />
+    </div>
+  ) : (
+    <section className="py-10  my-5  grid gap-8 grid-cols-1 lg:grid-cols-2 ">
+      {serviceData.title}
+    </section>
+  );
 };
 
 export default Service;
