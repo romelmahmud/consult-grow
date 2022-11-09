37e72784a55cf6e387c43e57fb6349d61a48ed9d
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-09 12:04:22 +0600

    Loding sippner adder on Home page

--- src/pages/Home/Components/Services/Services.jsx
+++ src/pages/Home/Components/Services/Services.jsx
@@ -1,13 +1,19 @@
+import { Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Service from "./Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     fetch("https://consult-review.vercel.app/limitservice")
       .then((res) => res.json())
-      .then((data) => setServices(data.data));
+      .then((data) => {
+        setServices(data.data);
+        setLoading(false);
+      });
   }, []);
 
   return (
@@ -15,11 +21,18 @@ const Services = () => {
       <h2 className="text-5xl font-bold text-center text-gray-900">
         My Services<span className="text-[#1cc65e] text-6xl ml-1 ">.</span>
       </h2>
-      <section className="py-10 6 my-5  grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {services?.map((service) => (
-          <Service key={service._id} service={service} />
-        ))}
-      </section>
+
+      {loading ? (
+        <div className="py-10  my-5 h-12 flex justify-center items-center">
+          <Spinner size="xl" color="success" />
+        </div>
+      ) : (
+        <section className="py-10  my-5  grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {services?.map((service) => (
+            <Service key={service._id} service={service} />
+          ))}
+        </section>
+      )}
 
       <div className="flex justify-center">
         <Link
