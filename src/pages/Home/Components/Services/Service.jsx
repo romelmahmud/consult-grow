491ea3c77ac7135c9482fe77cc34d967ef41c732
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-09 11:29:42 +0600

    home services section complete

---
+++ src/pages/Home/Components/Services/Service.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Service = ({ service }) => {
+  const { _id, title, description, price, img_url } = service;
+  return (
+    <article className="bg-white overflow-hidden rounded-lg border border-gray-100 shadow-sm">
+      <img alt={title} src={img_url} className="h-56 w-full object-cover" />
+
+      <div className="p-4 sm:p-6">
+        <Link to={`/services/${_id}`}>
+          <h3 className="text-2xl font-semibold text-gray-900 mb-5">{title}</h3>
+        </Link>
+        <p className="text-lg font-bold text-green-600">${price}</p>
+
+        <p className="mt-2 text-md leading-relaxed text-gray-700 line-clamp-3">
+          {description}
+        </p>
+
+        <Link
+          to={`/services/${_id}`}
+          className="group mt-4 inline-flex items-center gap-1 text-lg font-medium text-green-600 border-2 px-3 py-1 rounded border-green-600 hover:bg-green-600 hover:text-white transition "
+        >
+          Learn more
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default Service;
--- src/pages/Home/Components/Services/Services.jsx
+++ src/pages/Home/Components/Services/Services.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Service from "./Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("https://consult-review.vercel.app/service")
+    fetch("https://consult-review.vercel.app/limitservice")
       .then((res) => res.json())
       .then((data) => setServices(data.data));
   }, []);
@@ -13,11 +15,20 @@ const Services = () => {
       <h2 className="text-5xl font-bold text-center text-gray-900">
         My Services<span className="text-green-600 text-6xl ml-1 ">.</span>
       </h2>
-      <section className="py-10 md:py-16 my-5 md:my-10">
+      <section className="py-10 6 my-5  grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {services?.map((service) => (
-          <li key={service._id}>{service?.title}</li>
+          <Service key={service._id} service={service} />
         ))}
       </section>
+
+      <div className="flex justify-center">
+        <Link
+          to={`/services`}
+          className="  group  inline-flex items-center gap-1 text-xl font-medium hover:text-green-600 border-2 px-7 py-3 rounded border-green-600 bg-green-600 hover:bg-white text-white transition "
+        >
+          All Services
+        </Link>
+      </div>
     </section>
   );
 };
